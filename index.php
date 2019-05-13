4d8596dfd5af5092c4a0f4583d8937ff78f0a86b
Quill McConnell <quill8197@yahoo.com>
2019-05-13 16:25:31 -0400

    Added a survey route and populated it with checkboxes

--- index.php
+++ index.php
@@ -12,11 +12,20 @@ $f3 = Base::instance();
 //turn on Fat-Free error reporting
 $f3->set('DEBUG', 3);
 
+$f3->set('options', array('This midterm is easy', 'I like midterms', 'Today is Monday'));
+
 //Define a default route
 $f3->route('GET /', function()
 {
     echo '<h1>Midterm Survey</h1>';
     echo '<a href="survey">Take my midterm survey</a>';
+});
+
+//Define a survey route
+$f3->route('GET /survey', function()
+{
+    $view = new Template();
+    echo $view->render('views/survey.html');
 });
 
 //Run Fat-Free
